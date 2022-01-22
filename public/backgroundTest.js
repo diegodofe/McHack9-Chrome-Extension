992298e56a280ab90ac365baa54124353e9816d8
Diego Ferreira <diego.dorantes-ferreira@mail.mcgill.ca>
2022-01-21 23:05:24 -0500

    Add background script

---
+++ public/backgroundTest.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function backgroundTest() {
+  // const getCurrentTab = async () => {
+  //   console.log("getting tab");
+  //   let queryOptions = { active: true, currentWindow: true };
+  //   let [tab] = await chrome.tabs.query(queryOptions);
+  //   console.log(tab);
+  //   return tab;
+  // };
+}
--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  const getCurrentTab = async () => {
-    console.log("getting tab");
-    // let queryOptions = { active: true, currentWindow: true };
-    // let [tab] = await chrome.tabs.query(queryOptions);
-    // console.log("tab");
-    // return tab;
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <div>Alex Test</div>
-        <button onClick={getCurrentTab}>Test console</button>
+        <button>Test console</button>
       </header>
     </div>
   );
