711c921d3c59a4813d4bd8f5b0c658c958d9868e
Diego Ferreira <diego.dorantes-ferreira@mail.mcgill.ca>
2022-01-22 00:10:20 -0500

    Initegrate Boostrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo192 from "./logo192.png";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button>Test console</button>
+        <img src={logo192} className="App-logo" alt="brand-logo" />
+        <Button variant="success">Test console</Button>
       </header>
     </div>
   );
