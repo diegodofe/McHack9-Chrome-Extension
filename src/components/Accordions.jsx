30b5046e2b2f96ec0542c21d547d2572619e8d82
AlexFerence <alexference23@gmail.com>
2022-01-22 02:53:04 -0500

    Add score badge to companies

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -3,18 +3,28 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Accordion from "react-bootstrap/Accordion";
 
+import Badge from "react-bootstrap/Badge"
+
 export default function Accordions() {
+
+
+    let score = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+
     return (
         <Accordion>
             <Accordion.Item eventKey="0">
-                <Accordion.Header>Company #1</Accordion.Header>
+                <Accordion.Header>
+                    <h6>Company <Badge bg={(score > 50 ? "success" : "danger")}>{score}</Badge></h6>
+                </Accordion.Header>
                 <Accordion.Body>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
                     aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Company #2</Accordion.Header>
+                <Accordion.Header>
+                    <h6>Company <Badge bg={(score > 50 ? "success" : "danger")}>{score}</Badge></h6>
+                </Accordion.Header>
                 <Accordion.Body>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
                     aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
