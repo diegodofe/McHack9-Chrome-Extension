4bd993762cf7bcb9f791a3f4995b3cb47d0cf2b5
AlexFerence <alexference23@gmail.com>
2022-01-22 13:09:00 -0500

    getting urls

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 
 import logo192 from "../logo192.png";
 import Accordions from "./Accordions";
+import { useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
@@ -11,13 +12,69 @@ import axios from 'axios';
 
 
 export default function App() {
+
+  const [displayString, setDisplayString] = useState("");
+
+
   const testFunction = async () => {
     try {
       let queryOptions = { active: true, currentWindow: true };
       chrome.tabs.query(queryOptions, function (res) {
-        console.log(res[0].url)
 
+        const url = res[0].url
 
+        const urlSearchParams = new URLSearchParams(url);
+
+        //console.log(urlSearchParams.getAll());
+
+        console.log(urlSearchParams.get("k"));
+        console.log(urlSearchParams.get("sprefix"));
+        console.log(urlSearchParams.get("ref"));
+
+        var baseURl = ""
+
+        var isSearchTerm = false
+
+        var amazonSearchTerm = ""
+
+        var walmartSearchTem = ""
+
+        var slashCount = 0;
+
+        for (var x = 0; x < url.length - 1; x++) {
+          console.log(url.charAt(x));
+          if (url.charAt(x) === 'k' && url.charAt(x + 1) === '=') {
+            isSearchTerm = true
+            x++
+            x++
+          }
+          else if (url.charAt(x) === 'q' && url.charAt(x + 1) === '=') {
+            isSearchTerm = true
+            x++
+            x++
+          }
+          else if (url.charAt(x) === '&') {
+            isSearchTerm = false;
+          }
+          if (isSearchTerm) {
+            amazonSearchTerm = amazonSearchTerm + url.charAt(x).toString()
+            walmartSearchTem = walmartSearchTem + url.charAt(x).toString()
+          }
+
+          if (url.charAt(x) === '/') {
+            slashCount++;
+          }
+          else if (slashCount === 2) {
+            baseURl = baseURl + url.charAt(x);
+          }
+
+        }
+
+        console.log("search term")
+        console.log(amazonSearchTerm)
+        console.log(baseURl)
+        console.log(urlSearchParams.get('search'))
+        setDisplayString(baseURl)
 
         axios.get('http://127.0.0.1:5000/companycontroller/get', {
           params: {
@@ -62,6 +119,7 @@ export default function App() {
         <Card.Body>
           <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
           <Button variant="success" onClick={testFunction}>TEST FUNCTION</Button>
+          <div>{displayString}</div>
         </Card.Body>
         <Accordions />
       </Card>
