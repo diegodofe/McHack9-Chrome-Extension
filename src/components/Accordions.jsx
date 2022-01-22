728ab247f65fe97b1ca62a4eff8d6fc4e82a6e32
Diego Ferreira <diego.dorantes-ferreira@mail.mcgill.ca>
2022-01-22 11:34:23 -0500

    Create dynamic list of alternative companies

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -1,35 +1,23 @@
-import React from "react"
+import React from "react";
+import companies from "../companies";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Accordion from "react-bootstrap/Accordion";
-
-import Badge from "react-bootstrap/Badge"
+import Badge from "react-bootstrap/Badge";
 
 export default function Accordions() {
-
-
-    let score = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-
-    return (
-        <Accordion>
-            <Accordion.Item eventKey="0">
-                <Accordion.Header>
-                    <h6>Company <Badge bg={(score > 50 ? "success" : "danger")}>{score}</Badge></h6>
-                </Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>
-                    <h6>Company <Badge bg={(score > 50 ? "success" : "danger")}>{score}</Badge></h6>
-                </Accordion.Header>
-                <Accordion.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                    aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
-    );
+  return (
+    <Accordion>
+      {companies.map((company) => (
+        <Accordion.Item eventKey={company.key}>
+          <Accordion.Header>
+            <h6>
+              {company.name} <Badge bg={company.score > 50 ? "success" : "danger"}>{company.score}</Badge>
+            </h6>
+          </Accordion.Header>
+          <Accordion.Body>{company.content}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
 }
